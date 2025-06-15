f0098352287c4010b40d28e87b3825c2e5fba819
AG <me@amgsus.net>
2025-06-15 11:41:10 +0300

    Add support for switches. Add constant color mode

---
+++ src/README.md
@@ -0,0 +1,25 @@
+# Bicycle Light Controller (RP2040-based)
+
+## Controls
+
+### Red Switch
+
+The red-marked switch cycles through different light modes. These are:
+
+- Constant color
+- HUE-wheel (dynamic)
+
+### Green Switch
+
+The green-marked switch toggles emitting color. These are:
+
+- Red
+- Green
+- Blue
+- Purple
+- Yellow
+- Cyan
+
+### Blue Switch
+
+The blue-marked switch has no function yet.
--- src/index.js
+++ src/index.js
@@ -8,72 +8,140 @@ const LENGTH = NUM_DOWN_TUBE_LEDS;
 
 const np = new NeoPixel(PIN, LENGTH);
 
-// const colors = [
-//   np.color(0xff, 0, 0), // red
-//   np.color(0, 0xff, 0), // green
-//   np.color(0, 0, 0xff), // blue
-//   np.color(0xff, 0xff, 0), // yellow
-//   np.color(0xff, 0, 0xff), // magenta
-//   np.color(0, 0xff, 0xff), // cyan
-//   np.color(0xff, 0xff, 0xff), // white
-// ];
+const BTN_RED = 2;
+const BTN_GREEN = 3;
+const BTN_BLUE = 4;
 
+const PREDEFINED_COLORS_COUNT = 6;
+const WHITE_COLOR = np.color(0xff, 0xff, 0xff);
+const NO_COLOR = np.color(0, 0, 0);
+
+const PREDEFINED_COLORS = [
+    np.color(0xff, 0, 0),       // Red
+    np.color(0, 0xff, 0),       // Green
+    np.color(0, 0, 0xff),       // Blue
+    np.color(0xff, 0xff, 0),    // Yellow
+    np.color(0xff, 0, 0xff),    // Magenta
+    np.color(0, 0xff, 0xff)     // Cyan
+];
+
+const MODE_CONSTANT = 0;
+const MODE_HUE = 1;
+
+const HUE_UPDATE_INTERVAL = 36 * 3; // ms
+
+let colorIndex = 1; // Green
+let emittingColor = PREDEFINED_COLORS[0];
+let mode = MODE_CONSTANT;
 let hueAngle = 0;
-let time = 36*3; // millisecond
+let updateTimer = null;
 
-setInterval(() => {
-    hueAngle = ( hueAngle + 1 ) % 360;
+(() => {
+    setWatch(buttonClick, BTN_RED, RISING, 50);
+    setWatch(buttonClick, BTN_GREEN, RISING, 50);
+    setWatch(buttonClick, BTN_BLUE, RISING, 50);
 
-    let [ r, g, b ] = hsl2rgb(hueAngle, 1.0, 0.5);
-    let color = np.color(r * 255, g * 255, b * 255);
+    requireUpdate();
+})();
+
+function
+nextColor()
+{
+    colorIndex = (colorIndex + 1) % PREDEFINED_COLORS_COUNT;
+    emittingColor = PREDEFINED_COLORS[colorIndex];
+}
+
+function
+buttonClick(pin)
+{
+    switch (pin) {
+        case BTN_RED:
+            if (mode === MODE_CONSTANT) {
+                mode = MODE_HUE;
+            } else if (mode === MODE_HUE) {
+                mode = MODE_CONSTANT;
+            }
+            requireUpdate();
+            break;
+        case BTN_GREEN:
+            if (mode === MODE_CONSTANT) {
+                nextColor();
+                requireUpdate();
+            }
+            break;
+        case BTN_BLUE:
+            // Not used
+            break;
+    }
+}
+
+function
+requireUpdate()
+{
+    if (updateTimer) {
+        clearInterval(updateTimer);
+    }
+
+    switch (mode) {
+        case MODE_CONSTANT:
+            setPixels(PREDEFINED_COLORS[colorIndex]);
+            enableConstantColor();
+            break;
+        case MODE_HUE:
+            hueAngle = 0;
+            enableHueWheel();
+            break;
+    }
+}
+
+function
+enableConstantColor()
+{
+    refreshPixels();
+    updateTimer = setInterval(refreshPixels, 5000);
+}
+
+function
+enableHueWheel()
+{
+    hueWheelUpdate();
+    updateTimer = setInterval(hueWheelUpdate, HUE_UPDATE_INTERVAL);
+}
+
+function
+setPixels(color) // Sets only odd LEDs to reduce power consumption
+{
+    let skip = false;
 
     for ( let i = 0; i < np.length; i++ ) {
-        np.setPixel(i, color);
+        np.setPixel(i, skip ? NO_COLOR : color);
+        skip = !skip;
     }
-    
+}
+
+function
+refreshPixels()
+{
     np.show();
-}, time);
+}
+
+function
+hueWheelUpdate()
+{
+    hueAngle = ( hueAngle + 1 ) % 360;
+    let [ r, g, b ] = hsl2rgb(hueAngle, 1.0, 0.5);
+    let color = np.color(r * 255, g * 255, b * 255);
+    setPixels(color);
+    refreshPixels();
+}
 
 // // input: h as an angle in [0,360] and s,l in [0,1] - output: r,g,b in [0,1]
 // // https://stackoverflow.com/questions/2353211/hsl-to-rgb-color-conversion
-function hsl2rgb(h,s,l) 
+
+function 
+hsl2rgb(h, s, l)
 {
-   let a=s*Math.min(l,1-l);
-   let f= (n,k=(n+h/30)%12) => l - a*Math.max(Math.min(k-3,9-k,1),-1);
-   return [f(0),f(8),f(4)];
-} 
-
-// https://gist.github.com/mjackson/5311256
-
-/**
- * Converts an HSV color value to RGB. Conversion formula
- * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
- * Assumes h, s, and v are contained in the set [0, 1] and
- * returns r, g, and b in the set [0, 255].
- *
- * @param   Number  h       The hue
- * @param   Number  s       The saturation
- * @param   Number  v       The value
- * @return  Array           The RGB representation
- */
-// function hsvToRgb(h, s, v) 
-// {
-//   var r, g, b;
-
-//   var i = Math.floor(h * 6);
-//   var f = h * 6 - i;
-//   var p = v * (1 - s);
-//   var q = v * (1 - f * s);
-//   var t = v * (1 - (1 - f) * s);
-
-//   switch (i % 6) {
-//     case 0: r = v, g = t, b = p; break;
-//     case 1: r = q, g = v, b = p; break;
-//     case 2: r = p, g = v, b = t; break;
-//     case 3: r = p, g = q, b = v; break;
-//     case 4: r = t, g = p, b = v; break;
-//     case 5: r = v, g = p, b = q; break;
-//   }
-
-//   return [ r * 255, g * 255, b * 255 ];
-// }
+   let a = s * Math.min(l, 1 - l);
+   const f = (n, k = (n + h / 30) % 12) => l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+   return [ f(0), f(8), f(4) ];
+}
